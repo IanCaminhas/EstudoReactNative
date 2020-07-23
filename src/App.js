383f7e700dcd68cc9546728b7dc65cbb670c7fc0
IanCaminhas <iancaminhas25@hotmail.com>
2020-07-23 18:29:02 -0300

    Componente com propriedades

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Primeiro from './components/Primeiro'
-import CompPadrao, { Comp1, Comp2} from './components/Multi'
+import MinMax from './components/MixMax'
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 
 export default ()=> (
     <View style={style.App}>
         <Text>{ 1 + 1 }</Text>
+        <MinMax min="3" max="20" />
+        <MinMax min="4" max="21" />
         <CompPadrao />
         <Comp1 />
         <Comp2 />
@@ -18,7 +21,8 @@ const style = StyleSheet.create({
        // backgroundColor:'#A00',
         flexGrow: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        padding: 20
     },
 
 })
--- src/components/estilo.js
+++ src/components/estilo.js
@@ -2,7 +2,8 @@ import { StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
     fontG: {
-        fontSize: 32
+        fontSize: 32,
+        textAlign: "center"
     }
 
 
