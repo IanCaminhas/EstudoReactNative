706cda42b670b19d0c9a08dba46ba96fe3679aa7
IanCaminhas <iancaminhas25@hotmail.com>
2020-07-23 19:09:04 -0300

    Desafio numero aleatorio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Aleatorio from './components/Aleatorio' 
 import Primeiro from './components/Primeiro'
 import MinMax from './components/MixMax'
 import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 
 export default ()=> (
     <View style={style.App}>
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
         <Text>{ 1 + 1 }</Text>
         <MinMax min="3" max="20" />
         <MinMax min="4" max="21" />
