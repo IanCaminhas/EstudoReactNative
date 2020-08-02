e1933577456f0e836e01de0fd8fed8ec6c602a0b
IanCaminhas <iancaminhas25@hotmail.com>
2020-08-02 10:26:37 -0300

    Comunicacao direta e indireta

--- src/components/indireta/Pai.js
+++ src/components/indireta/Pai.js
@@ -7,7 +7,6 @@ export default props =>{
     const[texto, setTexto] = useState('')
     const[num, setNum] = useState(0)
 
-
     function exibirValor(numero, texto){
         setNum(numero)
         setTexto(texto)
