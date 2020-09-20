0160695973440c753b3a67df9ebb81f66ff5e554
IanCaminhas <iancaminhas25@hotmail.com>
2020-09-20 12:40:00 -0300

    Componente de Classes:Estado e cuidado com this

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput  } from 'react-native'
 import Estilo from '../estilo'
 
 export default class Mega extends Component {
 
+    state = {
+        qtdeNumeros: this.props.qtdeNumeros
+    }
+
+    constructor(props){
+        super(props)
+        this.alterarQtdeNumero = this.alterarQtdeNumero.bind(this)
+    }
+
+    alterarQtdeNumero = (qtde)=> {
+        this.setState({ qtdeNumeros: qtde })
+    }
+
     render(){
+        
         return (
+        <>
             <Text style={Estilo.fontG}>
                 Gerador de Mega-Sena
-                {this.props.qtdeNumeros}
+                {this.state.qtdeNumeros}
             </Text>
+
+            <TextInput 
+                KeyboardType={'numeric'}
+                style={{borderBottomWidth:1}}
+                placeholder="Qtde Números"
+                value= {this.state.qtdeNumeros}
+                onChangeText = {this.alterarQtdeNumero}
+            />
+        </>
         )
     }
 
